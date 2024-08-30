db28062c32ab8eeb272c54407b3dd317980df159
AlfredoCarichi <carichibrayan@gmail.com>
2024-08-29 19:13:59 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -28,6 +28,65 @@ import matplotlib.pyplot as plt
 import imutils
 
 
+# Define some helper functions for downloading pretrained model
+# taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
+import requests
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)  
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+parser = argparse.ArgumentParser(description='SKYAR')
+
+def load_model(self):
+        cloud_model_location = "1jbDLmw_ZWjDgUlVGrVde4h68U1edPzdT"
+        save_dest = Path('model')
+        save_dest.mkdir(exist_ok=True)
+
+        f_checkpoint = Path("bestNDVI.pt")
+
+        if not f_checkpoint.exists():
+            with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+                from GD_download import download_file_from_google_drive
+                download_file_from_google_drive(cloud_model_location, f_checkpoint)
+
+        checkpoint = torch.load(f_checkpoint, map_location=device)
+        self.net_G.load_state_dict(checkpoint['model_G_state_dict'])
+        self.net_G.to(device)
+        self.net_G.eval()
+        
+        # To free memory!
+        del f_checkpoint
+        del checkpoint
+        
+#         return model    
+
+
+
+
 def deteccion(image):
     
     
